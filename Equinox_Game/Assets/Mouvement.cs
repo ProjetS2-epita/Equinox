2daec9dfad431113fa9a6c828915eb989e137eab
Hugojuggernot Maître Course <39633298+MrHugojuggernot@users.noreply.github.com>
2022-01-16 13:26:18 +0100

    Finish PlayerMoveSet

--- Equinox_Game/Assets/Mouvement.cs
+++ Equinox_Game/Assets/Mouvement.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Mouvement : MonoBehaviour
 {
-    public float speed = 12f;
+    public float speed = 500f;
     public CharacterController controller;
+    Vector3 velocity;
+    public float gravity = -9.81f;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float jumpHeight = 3f;
+
+    bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +25,28 @@ public class Mouvement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+       isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
     float x = Input.GetAxis("Horizontal");
     float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
-        
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+
+
     }
 }
